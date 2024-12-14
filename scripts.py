6dbd0bee2b6e4b1fde076fbee76094e987e5ada0
“gopal <gnavghare7@gmail.com>
2024-12-14 13:17:34 +0530

    scripts.py

---
+++ scripts.py
@@ -0,0 +1,14 @@
+print("hi hellow how are you im fine")
+
+
+
+a =int(input('enter number'))
+b = int(input('enter second number:'))
+
+def addition(a,b):
+        return print('a+b:',a+b)
+
+
+
+
+addition(a,b)
