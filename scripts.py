dc52881bfdce479a07dc53adf29c87c5b821c372
gopalnavgha <gnavghare7@gmail.com>
2024-12-14 14:57:54 +0530

    Update scripts.py by vehicle class

--- scripts.py
+++ scripts.py
@@ -1,36 +1,55 @@
-print("hi hellow how are you im fine")
 
 
+class Vehicle:
+    def __init__(self, brand, model, price, fuel_type):
+        self.brand = brand
+        self.model = model
+        self.price = price
+        self.fuel_type = fuel_type
 
-a =int(input('enter number'))
-b = int(input('enter second number:'))
+    def display_info(self):
+        print(f"Brand: {self.brand}")
+        print(f"Model: {self.model}")
+        print(f"Price: ₹{self.price}")
+        print(f"Fuel Type: {self.fuel_type}")
 
-def addition(a,b):
-        return print('a+b:',a+b)
+    def start_engine(self):
+        print(f"{self.model} चे इंजिन सुरू झाले आहे!")
+
+    def stop_engine(self):
+        print(f"{self.model} चे इंजिन बंद झाले आहे!")
+
+    def apply_brakes(self):
+        print(f"{self.model} चे ब्रेक्स लावले गेले आहेत!")
+
+    def honk(self):
+        print(f"{self.model} चा हॉर्न: Beep Beep!")
+
+# Tata Motors ची काही मॉडेल्स
+nexon = Vehicle("Tata Motors", "Nexon", 900000, "Petrol")
+harrier = Vehicle("Tata Motors", "Harrier", 1500000, "Diesel")
+punch = Vehicle("Tata Motors", "Punch", 600000, "Petrol")
+
+# गाड्यांची माहिती दाखवणे
+print("**Nexon Information**")
+nexon.display_info()
+nexon.start_engine()
+nexon.honk()
+nexon.apply_brakes()
+nexon.stop_engine()
+
+print("\n**Harrier Information**")
+harrier.display_info()
+harrier.start_engine()
+harrier.honk()
+harrier.apply_brakes()
+harrier.stop_engine()
+
+print("\n**Punch Information**")
+punch.display_info()
+punch.start_engine()
+punch.honk()
+punch.apply_brakes()
+punch.stop_engine()
 
 
-
-
-addition(a,b)
-
-class car:
-        brand ='tata'
-        def __init__(self,model,noc):
-                self.model = model
-                self.noc = noc
-        def display(self):
-                print('Car brand is:',self.brand)
-                print('model_no:',self.model)
-                print('noc_no:',self.noc)
-
-obj = car('indica104','ABCSD1233ASDFD')
-print(obj.display())
-print(obj.brand)
-print(obj.model)
-print(obj.noc)
-
-
-
-
-
-print('this changes done by gopal navghare')
